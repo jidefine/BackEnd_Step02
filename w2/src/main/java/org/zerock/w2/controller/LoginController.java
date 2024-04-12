f80e9c2ad392e31fc779dcbdaf7a3505d686de2c
jidefine <youjio2000@gmail.com>
2024-04-12 11:53:38 +0900

    조회한 ToDO 확인 기능 구현

--- w2/src/main/java/org/zerock/w2/controller/LoginController.java
+++ w2/src/main/java/org/zerock/w2/controller/LoginController.java
@@ -6,10 +6,7 @@ import org.zerock.w2.service.MemberService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -31,10 +28,37 @@ public class LoginController extends HttpServlet {
 
         String mid = req.getParameter("mid");
         String mpw = req.getParameter("mpw");
+        // UUID로 자동 로그인 할 것인지 여부
         String auto = req.getParameter("auto");
 
+        // login.jsp에 name=auto가 check되어 있다면, rememberMe는 true
+        boolean rememberMe = auto != null && auto.equals("on");
+
         try{
+            // 기존 로그인
+            // id/pwd가 일치할 경우 memberDTO를 session공간에 저장한다.
             MemberDTO memberDTO = MemberService.INSTANCE.login(mid, mpw);
+
+            // 자동 로그인 설정이 되어 있는 경우
+            if(rememberMe){
+                // 유니크한 문자열을 생성할 때 사용
+                // ex) ba65870f-9b4f-4146-9cd8-58bd11baf5de
+                String uuid = UUID.randomUUID().toString();
+
+                // Maria-DB에 uuid를 저장
+                MemberService.INSTANCE.updateUuid(mid, uuid);
+                // loginInfo로 사용할 memberDTO에 uuid저장
+                memberDTO.setUuid(uuid);
+
+                // Maria-DB에 uuid를 저장
+                Cookie rememberCookie = new Cookie("remember-me", uuid);
+                rememberCookie.setMaxAge(60*60*24*7); // 쿠키의 유효 기간은 7일
+                rememberCookie.setPath("/");
+
+                // 브라우저에 응답보낼 때 remember-me를 전송해라
+                resp.addCookie(rememberCookie);
+            }
+            // 세션공간에 memberDTO를 loginInfo라는 key값으로 저장
             HttpSession session = req.getSession();
             session.setAttribute("logininfo", memberDTO);
             resp.sendRedirect(("/todo/list"));
@@ -49,10 +73,5 @@ public class LoginController extends HttpServlet {
         // 임시로 id+pw의 문자열을 loginInfo로 session에 저장
         session.setAttribute("logininfo", str);
         resp.sendRedirect("/todo/list");*/
-
-        boolean rememberMe = auto != null && auto.equals("on");
-        if(rememberMe){
-            String uuid = UUID.randomUUID().toString();
-        }
     }
 }
