aa3ae52741933c51776c584e07b14b5559aa0bd2
jidefine <youjio2000@gmail.com>
2024-04-15 12:13:33 +0900

    스프링 MVC 설정 완료

--- springex/src/main/java/org/zerock/springex/sample/EventSampleDAOImpl.java
+++ springex/src/main/java/org/zerock/springex/sample/EventSampleDAOImpl.java
@@ -1,11 +1,11 @@
 package org.zerock.springex.sample;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 @Repository
-//@Primary
 @Qualifier("event")
+//@Primary
 public class EventSampleDAOImpl implements SampleDAO{
 }
