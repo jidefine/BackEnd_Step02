fc8423c776d49d19c6cf2b4dbea70c3133dbde89
jidefine <youjio2000@gmail.com>
2024-04-16 13:45:50 +0900

    register.jsp에서 검증 에러 메시지 확인

--- springex/src/main/java/org/zerock/springex/controller/TodoController.java
+++ springex/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -2,12 +2,15 @@ package org.zerock.springex.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.springex.dto.TodoDTO;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/todo")
@@ -38,8 +41,15 @@ public class TodoController {
     TodoDTO내부의 필드들의 이름과 매칭되면 todoDTO객체 내부에 저장된다.
     * */
     @PostMapping("/register")
-    public String registerPost(TodoDTO todoDTO, RedirectAttributes redirectAttributes){
+    public String registerPost(@Valid TodoDTO todoDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
         log.info("POST todo register......");
+
+        if(bindingResult.hasErrors()){
+            log.info("has errors......");
+            redirectAttributes.addFlashAttribute("errors",
+                    bindingResult.getAllErrors());
+            return "redirect:/todo/register";
+        }
         log.info(todoDTO);
         return "redirect:/todo/list";
     }
