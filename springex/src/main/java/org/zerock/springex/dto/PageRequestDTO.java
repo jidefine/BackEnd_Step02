5a589ff198bf51bc08223571d83fb75a3916ba82
jidefine <youjio2000@gmail.com>
2024-04-17 11:56:48 +0900

    페이지 번호 계산

--- springex/src/main/java/org/zerock/springex/dto/PageRequestDTO.java
+++ springex/src/main/java/org/zerock/springex/dto/PageRequestDTO.java
@@ -15,14 +15,15 @@ public class PageRequestDTO {
     @Builder.Default
     @Min(value = 1)
     @Positive
-    private int page = 1;
+    private int page = 1; // 페이지 번호
 
     @Builder.Default
     @Min(value = 10)
     @Max(value = 100)
     @Positive
-    private int size = 10;
+    private int size = 10;  // 페이지 당 보여줄 정보의 크기
 
+    // 해당 페이지 이전에 몇개를 건너띄어야 하는지 계산하는 메서드
     public int getSkip(){
         return (page -1) * 10;
     }
