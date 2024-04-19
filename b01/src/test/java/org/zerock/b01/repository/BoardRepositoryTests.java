56a2429717de6a280121612458d061d70c0fef0e
jidefine <youjio2000@gmail.com>
2024-04-19 11:35:59 +0900

    페이징 처리

--- b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
+++ b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -4,8 +4,13 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.b01.domain.Board;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -53,8 +58,26 @@ public class BoardRepositoryTests {
 
     @Test
     public void testDelete(){
-        Long bno = 100L;
+        Long bno = 1L;
 
         boardRepository.deleteById(bno);
+    }
+
+    @Test
+    public void testPaging(){
+        // 1 page order by bno desc
+        //
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+        //
+        Page<Board> result = boardRepository.findAll(pageable);
+
+        log.info("total count: "+result.getTotalElements());
+        log.info("total pages: "+result.getTotalPages());
+        log.info("page number: "+result.getNumber());
+        log.info("page size: "+result.getSize());
+
+        List<Board> todoLiat = result.getContent();
+
+        todoLiat.forEach(board -> log.info(board));
     }
 }
