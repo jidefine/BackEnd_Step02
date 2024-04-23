10da7ba48935c8b0b14b871ae186b1ff715d5758
jidefine <youjio2000@gmail.com>
2024-04-23 20:16:16 +0900

    board 화면에 보여줄 댓글 갯수정보를 추가한 기능 구현  테스트 전

--- b01/src/main/java/org/zerock/b01/repository/search/BoardSearch.java
+++ b01/src/main/java/org/zerock/b01/repository/search/BoardSearch.java
@@ -3,6 +3,7 @@ package org.zerock.b01.repository.search;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.zerock.b01.domain.Board;
+import org.zerock.b01.dto.BoardListReplyCountDTO;
 
 /*QuertDsl을 사용하기 위해서 작성
 왜 필요하느냐?
@@ -19,4 +20,7 @@ public interface BoardSearch {
     Page<Board> search1(Pageable pageable);
 
     Page<Board> searchAll(String[] types, String keyword, Pageable pageable);
+
+    Page<BoardListReplyCountDTO> searchWithReplyCount(
+String[] types, String keyword, Pageable pageable);
 }
--- b01/src/main/java/org/zerock/b01/repository/search/BoardSearchImpl.java
+++ b01/src/main/java/org/zerock/b01/repository/search/BoardSearchImpl.java
@@ -1,6 +1,7 @@
 package org.zerock.b01.repository.search;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -8,6 +9,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.zerock.b01.domain.Board;
 import org.zerock.b01.domain.QBoard;
+import org.zerock.b01.domain.QReply;
+import org.zerock.b01.dto.BoardListReplyCountDTO;
 
 import java.util.List;
 
@@ -63,6 +66,57 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
             long count = query.fetchCount();
             return new PageImpl<>(list, pageable,count);
 
+    }
+
+    @Override
+    public Page<BoardListReplyCountDTO> searchWithReplyCount(String[] types, String keyword, Pageable pageable) {
+        // Querydsl -> JPQL로 변환
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+
+        // 왼쪽 테이블인 board에 기준을 맞춰서 board는 다 출력줘
+        // board의 row(행)을 댓글이 존재하지 않아도 모두 출력해줘
+        // FROM board LEFT OUTER JOIN reply ON reply.board_bno = board.bno;
+
+        // FROM board;
+        JPQLQuery<Board>query = from(board);
+
+        // LEFT OUTER JOIN reply ON reply.board_bno = board.bno
+        query.leftJoin(reply).on(reply.board.eq(board));
+
+        //GROUB BY board의 컬럼들
+        query.groupBy(board);
+
+        if((types != null && types.length > 0) && keyword != null){
+            BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+            for(String type: types){
+                switch (type){
+                    case "t":
+                        booleanBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "c":
+                        booleanBuilder.or(board.title.contains(keyword));
+                    case "w":
+                        booleanBuilder.or(board.content.contains(keyword));
+                        break;
+                }
+            }
+            query.where(booleanBuilder);
+        }
+        query.where(board.bno.gt(0L));
+
+        // SELECT bno, title, writer, regDate, COUNT(reply) replyCount
+        JPQLQuery<BoardListReplyCountDTO>dtoQuery = query.select(Projections
+                .bean(BoardListReplyCountDTO.class,
+                        board.bno,
+                        board.title,
+                        board.writer,
+                        board.regDate,
+                        reply.count().as("replyCount")
+                ));
+
+        return null;
     }
 
 }
