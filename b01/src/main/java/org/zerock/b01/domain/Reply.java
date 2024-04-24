28cd80336f543fefc41e11e1b798c52632298f60
jidefine <youjio2000@gmail.com>
2024-04-24 11:35:22 +0900

    댓글 조회/수정/삭제/목록 구현

--- b01/src/main/java/org/zerock/b01/domain/Reply.java
+++ b01/src/main/java/org/zerock/b01/domain/Reply.java
@@ -65,4 +65,8 @@ public class Reply extends BaseEntity{
     private String replyText;
 
     private String replyer;
+
+    public void changeText(String text){
+        this.replyText = text;
+    }
 }
