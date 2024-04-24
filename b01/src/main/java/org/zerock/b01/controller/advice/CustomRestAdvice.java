004256fd74dd83ef29c5fd36bc3fcc1552c22c9e
jidefine <youjio2000@gmail.com>
2024-04-24 13:47:26 +0900

    특정 댓글 조회 기능 구현 데이터가 존재하지 않을 시 로그 메시지로 처리

--- b01/src/main/java/org/zerock/b01/controller/advice/CustomRestAdvice.java
+++ b01/src/main/java/org/zerock/b01/controller/advice/CustomRestAdvice.java
@@ -2,6 +2,7 @@ package org.zerock.b01.controller.advice;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -10,9 +11,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 @Log4j2
@@ -45,6 +46,18 @@ public class CustomRestAdvice {
 
         errorMap.put("time", ""+System.currentTimeMillis());
         errorMap.put("msg", "constaint fails");
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+
+    @ExceptionHandler({NoSuchElementException.class, EmptyResultDataAccessException.class})
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handleNoSuchElement(Exception e){
+        log.error(e);
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("time", "" + System.currentTimeMillis());
+        errorMap.put("msg", "NO Such Element Exception");
         return ResponseEntity.badRequest().body(errorMap);
     }
 }
