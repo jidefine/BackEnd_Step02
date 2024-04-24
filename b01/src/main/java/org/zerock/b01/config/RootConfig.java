51ee42e6ee45a99917f20c04766517702d106309
jidefine <youjio2000@gmail.com>
2024-04-24 14:02:56 +0900

    MatchingStrategies.LOOSE에 대한 설명 추가

--- b01/src/main/java/org/zerock/b01/config/RootConfig.java
+++ b01/src/main/java/org/zerock/b01/config/RootConfig.java
@@ -8,13 +8,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RootConfig {
 
+    // 어떤 object에 있는 필드값들을 자동으로 언하는 object로 mapping시켜줌
     @Bean
     public ModelMapper getMapper() {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration()
                 .setFieldMatchingEnabled(true)
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
-                .setMatchingStrategy(MatchingStrategies.STRICT);
+                .setMatchingStrategy(MatchingStrategies.LOOSE);
+                // source 속성을 destination 속성만 일치하도록 함
 
         return modelMapper;
     }
