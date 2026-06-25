8621002f443d77a4edc4200518f1793282de91e0
jidefine <148775766+jidefine@users.noreply.github.com>
2026-06-25 23:59:26 +0900

    CRUD Setting

--- 2026_jdbcex/src/test/java/org/zerock/dao/TodoDAOTests.java
+++ 2026_jdbcex/src/test/java/org/zerock/dao/TodoDAOTests.java
@@ -1,8 +1,13 @@
 package org.zerock.dao;
 
+import jdk.internal.vm.compiler.collections.EconomicMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.zerock.jdbcex.dao.TodoDAO;
+import org.zerock.jdbcex.domain.TodoVO;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class TodoDAOTests {
 
@@ -16,5 +21,40 @@ public class TodoDAOTests {
     @Test
     public void testTime() throws Exception {
         System.out.println(todoDAO.getTime());
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+        TodoVO todoVO = TodoVO.builder()
+                .title("Sample Title...")
+                .dueDate(LocalDate.of(2026,06,25))
+                .build();
+
+        todoDAO.insert(todoVO);
+    }
+
+    @Test
+    public void testList() throws Exception {
+        List<TodoVO> list = todoDAO.selectAll();
+        list.forEach(vo -> System.out.println(vo));
+    }
+
+    @Test
+    public void testSelectOne() throws Exception {
+        Long tno = 2L;
+        TodoVO vo = todoDAO.selectOne(tno);
+        System.out.println(vo);
+    }
+
+    @Test
+    public void testUpdateOne() throws Exception {
+        TodoVO todoVO = TodoVO.builder()
+                .tno(2L)
+                .title("Todo Title..")
+                .dueDate(LocalDate.of(2026,06,25))
+                .finished((true))
+                .build();
+
+        todoDAO.updateOne(todoVO);
     }
 }
