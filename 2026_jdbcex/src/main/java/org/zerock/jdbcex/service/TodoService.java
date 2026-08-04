a5cc163d518367b32bc9cafe6ed823a5e6905718
jidefine <148775766+jidefine@users.noreply.github.com>
2026-08-04 23:18:55 +0900

    feat: add todo modify view

--- 2026_jdbcex/src/main/java/org/zerock/jdbcex/service/TodoService.java
+++ 2026_jdbcex/src/main/java/org/zerock/jdbcex/service/TodoService.java
@@ -48,4 +48,12 @@ public enum TodoService {
         TodoDTO todoDTO = modelMapper.map(todoVO, TodoDTO.class);
         return todoDTO;
     }
+
+    public void modify (TodoDTO todoDTO) throws Exception {
+        log.info("todoDTO: " + todoDTO);
+
+        TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
+
+        dao.updateOne(todoVO);
+    }
 }
