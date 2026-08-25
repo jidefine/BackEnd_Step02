2471037c9d8d3b7c2656b95796af6f096cc7e003
jidefine <148775766+jidefine@users.noreply.github.com>
2026-08-25 23:34:48 +0900

    ApplicationContext and Dependency Injection

--- 2026_springex/src/main/java/org/zerock/_026_springex/sample/SampleDAO.java
+++ 2026_springex/src/main/java/org/zerock/_026_springex/sample/SampleDAO.java
@@ -1,4 +1,7 @@
 package org.zerock._026_springex.sample;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class SampleDAO {
 }
--- 2026_springex/src/main/java/org/zerock/_026_springex/sample/SampleService.java
+++ 2026_springex/src/main/java/org/zerock/_026_springex/sample/SampleService.java
@@ -1,4 +1,14 @@
 package org.zerock._026_springex.sample;
 
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@ToString
+@RequiredArgsConstructor
 public class SampleService {
+    @Autowired
+    private final SampleDAO sampleDAO;
 }
